1a7d8d3ff1cf09ebfc7ba93c4e4a91002bba0a83
Davi Henrique Amaral <imyuno2022@outlook.com>
2026-08-07 15:41:34 -0300

    feat: persist users with SQLite

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -1,7 +1,45 @@
 import { database } from '../database/database.js';
 
-const users = [];
-let nextUserID = 1;
+const getAllUsersStatement = database.prepare(`
+    SELECT id, name, email, age
+    FROM users
+    ORDER BY id
+`);
+
+const getUserByIDStatement = database.prepare(`
+    SELECT id, name, email, age
+    FROM users
+    WHERE id = ?
+`);
+
+const createUserStatement = database.prepare(`
+    INSERT INTO users (name, email, age)
+    VALUES (?, ?, ?)
+    RETURNING id, name, email, age
+`);
+
+const replaceUserStatement = database.prepare(`
+    UPDATE users
+    SET name = ?, email = ?, age = ?
+    WHERE id = ?
+    RETURNING id, name, email, age
+`);
+
+const updateUserStatement = database.prepare(`
+    UPDATE users
+    SET
+        name = COALESCE(?, name),
+        email = COALESCE(?, email),
+        age = COALESCE(?, age)
+    WHERE id = ?
+    RETURNING id, name, email, age
+`);
+
+const deleteUserStatement = database.prepare(`
+    DELETE FROM users
+    WHERE id = ?
+    RETURNING id, name, email, age
+`);
 
 export function getAllUsers() {
     return getAllUsersStatement.all();
@@ -25,7 +63,7 @@ export function replaceUser(id, userData) {
         userData.email,
         userData.age,
         id
-    )
+    );
 }
 
 export function updateUser(id, userData) {
@@ -34,57 +72,9 @@ export function updateUser(id, userData) {
         userData.email ?? null,
         userData.age ?? null,
         id
-    )
+    );
 }
 
 export function deleteUser(id) {
-    const userIndex = users.findIndex(user => user.id === id);
-
-    if (userIndex === -1) {
-        return null;
-    }
-
-    const deletedUsers = users.splice(userIndex, 1);
-    const deletedUser = deletedUsers[0];
-
-    return deletedUser;
+    return deleteUserStatement.get(id);
 }
-
-const getAllUsersStatement = database.prepare(`
-    SELECT id, name, email, age
-    FROM users
-    ORDER BY id
-`);
-
-const createUserStatement = database.prepare(`
-    INSERT INTO users (name, email, age)
-    VALUES (?, ?, ?)
-    RETURNING id, name, email, age
-`);
-
-const getUserByIDStatement = database.prepare(`
-    SELECT id, name, email, age
-    FROM users
-    WHERE id = ?
-`);
-
-const replaceUserStatement = database.prepare(`
-    UPDATE users
-    SET name = ?, email = ?, age = ?
-    WHERE id = ? 
-    RETURNING id, name, email, age
-`);
-
-const updateUserStatement = database.prepare(`
-    UPDATE users
-    SET
-        name = COALESCE(?, name),
-        email = COALESCE(?, email),
-        age = COALESCE(?, age)
-    WHERE id = ?
-    RETURNING id, name, email, age
-`)
-
-const deleteUserStatement = database.prepare(`
-    
-`)
